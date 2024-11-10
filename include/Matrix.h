4c2b6deedcf358910c4fecdbda0f37bca3dc236b
Georgios Tsolakidis <rossakis@hotmail.com>
2024-11-10 23:35:21 +0200

    Started reworking the library to be more in-line with the necessities of a graphics framework (OpenGL, Vulkan)

--- include/Matrix.h
+++ include/Matrix.h
@@ -6,22 +6,23 @@
 #define MATRICES_H
 #include <MathG.h>
 
+//TODO: incorporate vectors into the possible operations with matrices
 namespace MathG{
-    struct Matrix2x2 {
+    struct Matrix2 {
         float m11;
         float m12;
         float m21;
         float m22;
 
         ///Identity(2) matrix
-        static const Matrix2x2 ID;
+        static const Matrix2 ID;
 
-        explicit Matrix2x2(const float value) : Matrix2x2(value, value, value, value) {}
+        explicit Matrix2(const float value) : Matrix2(value, value, value, value) {}
 
-        explicit Matrix2x2(const float m1, const float m2, const float m3, const float m4) : m11(m1), m12(m2), m21(m3), m22(m4) {
+        explicit Matrix2(const float m1, const float m2, const float m3, const float m4) : m11(m1), m12(m2), m21(m3), m22(m4) {
         }
 
-        Matrix2x2& operator = (const Matrix2x2 matrix) {
+        Matrix2& operator = (const Matrix2 matrix) {
             if(this != &matrix) {
                 this->m11 = matrix.m11;
                 this->m12 = matrix.m12;
@@ -31,15 +32,15 @@ namespace MathG{
             return *this;
         }
 
-        Matrix2x2 operator + (const Matrix2x2 &matrix) const {
-            return Matrix2x2(this->m11 + matrix.m11,
+        Matrix2 operator + (const Matrix2 &matrix) const {
+            return Matrix2(this->m11 + matrix.m11,
                 this-> m12 + matrix.m12,
                 this-> m21 + matrix.m21,
                 this-> m22 + matrix.m22);
         }
 
-        Matrix2x2 operator - (const Matrix2x2 &matrix) const {
-            return Matrix2x2(this->m11 - matrix.m11,
+        Matrix2 operator - (const Matrix2 &matrix) const {
+            return Matrix2(this->m11 - matrix.m11,
                 this-> m12 - matrix.m12,
                 this-> m21 - matrix.m21,
                 this-> m22 - matrix.m22);
@@ -62,8 +63,8 @@ namespace MathG{
             }
         }
 
-        Matrix2x2 operator * (const Matrix2x2 &matrix) const {
-            return Matrix2x2((this->m11 * matrix.m11) + (this->m12 * matrix.m21),
+        Matrix2 operator * (const Matrix2 &matrix) const {
+            return Matrix2((this->m11 * matrix.m11) + (this->m12 * matrix.m21),
                              (this->m11 * matrix.m12) + (this->m12 * matrix.m22),
                              (this->m21 * matrix.m11) + (this->m22 * matrix.m21),
                              (this->m21 * matrix.m12) + (this->m22 * matrix.m22));
@@ -72,36 +73,36 @@ namespace MathG{
         //TODO: add operator * matrix2x2 with vector2
 
         ///Returns a transposed instance of this matrix.
-        [[nodiscard]] Matrix2x2 Transpose() const{
-            return Matrix2x2(this->m11, this->m21, this->m12, this->m22);
+        [[nodiscard]] Matrix2 Transpose() const{
+            return Matrix2(this->m11, this->m21, this->m12, this->m22);
         }
 
-        [[nodiscard]] static Matrix2x2 Rotate(float degrees) {
-            return Matrix2x2(cos(degrees),
+        [[nodiscard]] static Matrix2 Rotate(float degrees) {
+            return Matrix2(cos(degrees),
                              sin(degrees),
                              -sin(degrees),
                              cos(degrees));
         }
 
         ///Uniform scale
-        [[nodiscard]] Matrix2x2 Scale(float scalar) const{
-            return Matrix2x2(this->m11 * scalar, this->m12, this->m21, this->m22 * scalar);
+        [[nodiscard]] Matrix2 Scale(float scalar) const{
+            return Matrix2(this->m11 * scalar, this->m12, this->m21, this->m22 * scalar);
         }
 
         ///Non-Uniform scale
-        [[nodiscard]] Matrix2x2 Scale(float scalarX, float scalarY) const {
-            return Matrix2x2(this->m11 * scalarX, this->m12, this->m21, this->m22 * scalarY);
+        [[nodiscard]] Matrix2 Scale(float scalarX, float scalarY) const {
+            return Matrix2(this->m11 * scalarX, this->m12, this->m21, this->m22 * scalarY);
         }
 
-        [[nodiscard]] static Matrix2x2 SkewX(float scalarX) {
-            return Matrix2x2(1, 0, scalarX, 1);
+        [[nodiscard]] static Matrix2 SkewX(float scalarX) {
+            return Matrix2(1, 0, scalarX, 1);
         }
 
-        [[nodiscard]] static Matrix2x2 SkewY(float scalarY) {
-            return Matrix2x2(1, scalarY, 0, 1);
+        [[nodiscard]] static Matrix2 SkewY(float scalarY) {
+            return Matrix2(1, scalarY, 0, 1);
         }
 
-        [[nodiscard]] static Matrix2x2 SkewXY(float scalarX, float scalarY) {
+        [[nodiscard]] static Matrix2 SkewXY(float scalarX, float scalarY) {
             return SkewX(scalarX) * SkewY(scalarY);
         }
         //TODO:
@@ -130,25 +131,32 @@ namespace MathG{
             return ss.str();
         }
     };
-    const Matrix2x2 Matrix2x2::ID = Matrix2x2(0.0f, 0.0f, 0.0f, 0.0f);
+    const Matrix2 Matrix2::ID = Matrix2(0.0f, 0.0f, 0.0f, 0.0f);
 
     //TODO: Add Matrix3x3
 
-    /*struct Matrix3x3 {
+    struct Matrix3 {
         float m11;
         float m12;
+        float m13;
         float m21;
         float m22;
+        float m23;
+        float m31;
+        float m32;
+        float m33;
 
         ///Identity(2) matrix
-        static const Matrix2x2 ID;
+        static const Matrix3 ID;
 
-        explicit Matrix2x2(const float value) : Matrix2x2(value, value, value, value) {}
+        explicit Matrix3(const float value) : Matrix3(value, value, value, value, value, value, value, value, value) {}
 
-        explicit Matrix2x2(const float m1, const float m2, const float m3, const float m4) : m11(m1), m12(m2), m21(m3), m22(m4) {
+        explicit Matrix3
+        (const float m11, const float m12, const float m13, const float m21, const float m22, const float m23, const float m31, const float m32, const float m33) : 
+        m11(m11), m12(m12), m21(m13), m22(m21), m22(m22), m22(m23), m22(m31), m22(m32), m22(m33) {
         }
 
-        Matrix2x2& operator = (const Matrix2x2 matrix) {
+        Matrix3& operator = (const Matrix3 matrix) {
             if(this != &matrix) {
                 this->m11 = matrix.m11;
                 this->m12 = matrix.m12;
@@ -158,88 +166,120 @@ namespace MathG{
             return *this;
         }
 
-        Matrix2x2 operator + (const Matrix2x2 &matrix) const {
+        Matrix3 operator + (const Matrix3 &matrix) const {
             return Matrix2x2(this->m11 + matrix.m11,
                              this-> m12 + matrix.m12,
                              this-> m21 + matrix.m21,
                              this-> m22 + matrix.m22);
         }
 
-        Matrix2x2 operator - (const Matrix2x2 &matrix) const {
+        Matrix3 operator - (const Matrix3 &matrix) const {
             return Matrix2x2(this->m11 - matrix.m11,
                              this-> m12 - matrix.m12,
                              this-> m21 - matrix.m21,
                              this-> m22 - matrix.m22);
         }
 
-        //For more modular access to the field members
-        float& operator[](const int index) {
+        //For more modular access to the field members, e.g. type 1 to get member m11 of matrix
+        float& operator[](const int firstIndex, const int secondIndex) {
             switch (index) {
                 case 1:
-                    return this->m11;
+                    switch (secondIndex)
+                    {
+                    case 1:
+                        return this->m11;
+                    case 2:
+                        return this->m12;
+                    case 3:
+                        return this->m13;
+                    default:
+                        break;
+                    }
                 case 2:
-                    return this->m12;
+                    switch (secondIndex)
+                    {
+                    case 1:
+                        return this->m21;
+                    case 2:
+                        return this->m22;
+                    case 3:
+                        return this->m23;
+                    default:
+                        break;
+                    }
                 case 3:
-                    return this->m21;
-                case 4:
-                    return this->m22;
+                    switch (secondIndex)
+                    {
+                    case 1:
+                        return this->m31;
+                    case 2:
+                        return this->m32;
+                    case 3:
+                        return this->m33;
+                    default:
+                        break;
+                    }
                 default:
-                    //When passing wrong indices
                     throw InvalidParametersException("Nonexistent matrix coordinates passed as parameters!");
             }
         }
 
-        Matrix2x2 operator * (const Matrix2x2 &matrix) const {
-            return Matrix2x2((this->m11 * matrix.m11) + (this->m12 * matrix.m21),
-                             (this->m11 * matrix.m12) + (this->m12 * matrix.m22),
-                             (this->m21 * matrix.m11) + (this->m22 * matrix.m21),
-                             (this->m21 * matrix.m12) + (this->m22 * matrix.m22));
+        Matrix3 operator * (const Matrix3 &matrix) const {
+            return Matrix3(
+                (this->m11 * matrix.m11) + (this->m12 * matrix.m21),
+                (this->m11 * matrix.m12) + (this->m12 * matrix.m22),
+                (this->m21 * matrix.m11) + (this->m22 * matrix.m21),
+                (this->m21 * matrix.m12) + (this->m22 * matrix.m22));
         }
 
         ///Returns a transposed instance of this matrix.
-        [[nodiscard]] Matrix2x2 Transpose() const{
+        [[nodiscard]] Matrix3 Transpose() const{
             return Matrix2x2(this->m11, this->m21, this->m12, this->m22);
         }
 
-        [[nodiscard]] static Matrix2x2 Rotate(float degrees) {
-            return Matrix2x2(cos(degrees),
+        [[nodiscard]] static Matrix3 Rotate(float degrees) {
+            return Matrix3(cos(degrees),
                              sin(degrees),
                              -sin(degrees),
                              cos(degrees));
         }
 
         ///Uniform scale
-        [[nodiscard]] Matrix2x2 Scale(float scalar) const{
-            return Matrix2x2(this->m11 * scalar, this->m12, this->m21, this->m22 * scalar);
+        [[nodiscard]] Matrix3 Scale(float scalar) const{
+            return Matrix3(
+                this->m11 * scalar, 
+                this->m12, 
+                this->m21, 
+                this->m22 * scalar);
         }
 
         ///Non-Uniform scale
-        [[nodiscard]] Matrix2x2 Scale(float scalarX, float scalarY) const {
-            return Matrix2x2(this->m11 * scalarX, this->m12, this->m21, this->m22 * scalarY);
+        [[nodiscard]] Matrix3 Scale(float scalarX, float scalarY) const {
+            return Matrix3(this->m11 * scalarX, this->m12, this->m21, this->m22 * scalarY);
         }
 
-        [[nodiscard]] static Matrix2x2 SkewX(float scalarX) {
-            return Matrix2x2(1, 0, scalarX, 1);
+        [[nodiscard]] static Matrix3 SkewX(float scalarX) {
+            return Matrix3(1, 0, scalarX, 1);
         }
 
-        [[nodiscard]] static Matrix2x2 SkewY(float scalarY) {
-            return Matrix2x2(1, scalarY, 0, 1);
+        [[nodiscard]] static Matrix3 SkewY(float scalarY) {
+            return Matrix3(1, scalarY, 0, 1);
         }
 
-        [[nodiscard]] static Matrix2x2 SkewXY(float scalarX, float scalarY) {
+        [[nodiscard]] static Matrix3 SkewXY(float scalarX, float scalarY) {
             return SkewX(scalarX) * SkewY(scalarY);
         }
         //TODO:
-            Chapter-5
-        Rotate()
-        Scale(x,y) and Scale(k)  -> uniform scale
-        Project()
-        Reflect()
-        Skew()
-            Chapter-6
-        Determinant(): Calculates the determinant of the matrix.
-        Inverse(): Returns a new matrix that is the inverse of the current matrix (if it exists).
-        Utility functions for specific operations you might need, like rotation or scaling transformations (depending on your library's focus).
+        //     Chapter-5
+        // Rotate()
+        // Scale(x,y) and Scale(k)  -> uniform scale
+        // Project()
+        // Reflect()
+        // Skew()
+        //     Chapter-6
+        // Determinant(): Calculates the determinant of the matrix.
+        // Inverse(): Returns a new matrix that is the inverse of the current matrix (if it exists).
+        // Utility functions for specific operations you might need, like rotation or scaling transformations (depending on your library's focus).
 
 
         //DEBUG
@@ -250,14 +290,17 @@ namespace MathG{
         ///By default, shows the maximum amount of digits for each float number of this vector
         [[nodiscard]] std::string ToString(const int precisionNumber = 7) const {
             std::stringstream ss;
-            ss << std::fixed << std::setprecision(precisionNumber)
-               << "|" << this->m11 << ", " << this->m12 << "|\n"
-               << "|" << this->m21 << ", " << this->m22 << "|\n";
+            ss  << std::fixed << std::setprecision(precisionNumber)
+                << "|" << this->m11 << ", " << this->m12 << ", " << this->m13 << "|\n"
+                << "|" << this->m21 << ", " << this->m22 << ", " << this->m23 << "|\n"
+                << "|" << this->m31 << ", " << this->m32 << ", " << this->m33 << "|\n";
 
             return ss.str();
         }
     };
-    const Matrix2x2 Matrix2x2::ID = Matrix2x2(0.0f, 0.0f, 0.0f, 0.0f);*/
+    const Matrix3 Matrix3::ID = Matrix3(0.0f, 0.0f, 0.0f, 
+                               0.0f, 0.0f, 0.0f,
+                               0.0f, 0.0f, 0.0f);
 }
 
 #endif //MATRICES_H
--- include/Vector.h
+++ include/Vector.h
@@ -178,5 +178,124 @@ namespace  MathG {
         }
     };
     const Vector3 Vector3::zero = Vector3(0, 0, 0);
+
+    struct Vector4 {
+        float x;
+        float y;
+        float z;
+        float w;
+        float magnitude;
+        static const Vector4 zero;
+
+        explicit Vector4(const float xValue = 0, const float yValue = 0, const float zValue = 0, const float wValue = 0) : x(xValue), y(yValue), z(zValue), w(wValue) {
+            this->magnitude = std::sqrt(x * x + y * y + z * z + w * w);
+        }
+
+        static float Distance(const Vector4 &vec1, const Vector4 &vec2) {
+            return static_cast<float>(std::sqrt
+                (   std::pow(vec2.x - vec1.x, 2) +
+                    std::pow(vec2.y - vec1.y, 2) +
+                    std::pow(vec2.z - vec1.z, 2) +
+                    std::pow(vec2.w - vec1.w, 2)));
+        }
+
+        static float Dot(const Vector4 &vec1, const Vector4 &vec2) {
+            return vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z + vec1.w * vec2.w;
+        }
+
+        //TODO:
+        static Vector4 Cross(const Vector4 &vec1, const Vector4 &vec2) {
+            return Vector4(vec1.y * vec2.z - vec1.z * vec2.y, 
+            vec1.z * vec2.x - vec1.x * vec2.z, 
+            vec1.x * vec2.y - vec1.y * vec2.x);
+        }
+
+        static float Angle(const Vector4 &vec1, const Vector4 &vec2) {
+            if (vec1.magnitude == 0.0f || vec2.magnitude == 0.0f) {
+                std::cout << "Trying to find angle between two Vector4, when one of them is Vector4.zero" << std::endl;
+                return 0.0f;
+            }
+            return std::acos(Dot(vec1, vec2) / (vec1.magnitude * vec2.magnitude));
+        }
+
+        Vector4& operator = (const Vector4 &vec4) {
+            // Check for self-assignment
+            if (this != &vec4) {
+                this->x = vec4.x;
+                this->y = vec4.y;
+                this->z = vec4.z;
+                this->w = vec4.w;
+            }
+            return *this; // Return a reference to the current object
+        }
+
+        Vector4 operator + (const Vector4 &vec4) const {
+            return Vector4(
+                this->x + vec4.x, 
+                this->y + vec4.y, 
+                this->z + vec4.z, 
+                this->w + vec4.w);
+        }
+
+        Vector4 operator - (const Vector4 &vec4) const {
+            return Vector4(
+                this->x + vec4.x, 
+                this->y + vec4.y, 
+                this->z + vec4.z,
+                this->w + vec4.w);
+        }
+
+        Vector4 operator * (const float scalar) const {
+            return Vector4(
+                this->x * scalar, 
+                this->y * scalar, 
+                this->z * scalar, 
+                this->w * scalar);
+        }
+
+        void operator += (const Vector4 &vec4) {
+            this->x += vec4.x;
+            this->y += vec4.y;
+            this->z += vec4.z;
+            this->w += vec4.w;
+        }
+
+        void operator -= (const Vector4 &vec4) {
+            this->x -= vec4.x;
+            this->y -= vec4.y;
+            this->z -= vec4.z;
+            this->w -= vec4.z;
+        }
+
+        void operator *= (const Vector4 &vec4) {
+            this->x *= vec4.x;
+            this->y *= vec4.y;
+            this->z *= vec4.z;
+            this->w *= vec4.w;
+        }
+
+        bool operator == (const Vector4 &vec4) const {
+            return  this->x == vec4.x && 
+                    this->y == vec4.y && 
+                    this->z == vec4.z &&
+                    this->w == vec4.w;
+        }
+
+        bool operator != (const Vector4 &vec4) const {
+            return this->x != vec4.x 
+                || this->y != vec4.y 
+                || this->z != vec4.z 
+                || this->w != vec4.w;
+        }
+
+        ///By default, shows the maximum amount of digits for each float number of this vector
+        [[nodiscard]] std::string ToString(const int precisionNumber) const {
+            std::stringstream ss;
+            ss << std::fixed << std::setprecision(precisionNumber) 
+            << "(" << this->x << ", " << this->y << ", " << this->z << ", " << this->w << ")";
+            return ss.str();
+        }
+    };
+    const Vector4 Vector4::zero = Vector4(0, 0, 0, 0);
 }
 #endif //VECTOR_H
